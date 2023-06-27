ebcdd020cc2c57d19bf57c31f4d17f45191dca05
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-27 14:58:47 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,23 +25,23 @@ void bubble_sort(int *array, size_t size)
 {
 size_t i;
 size_t length;
-int b = 0;
+bool bubbly = false;
 
 if (array == NULL || size < 2)
 {
 return;
 }
 
-while (b == 0)
+while (!b)
 {
-b = 1;
+b = true;
 for (i = 0; i < length - 1; i++)
 {
 if (array[i] > array[i + 1])
 {
 swap_ints(array + i, array + i + 1);
 print_array(array, size);
-b = 0;
+b = false;
 }
 }
 length--;
