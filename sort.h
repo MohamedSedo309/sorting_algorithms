ea6341108a57febf56a63fbdfd981e058bd76a22
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-27 17:28:35 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -36,5 +36,8 @@ void swap_ints(int *x, int *y);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
